53a8a1657bc6f413d69a53133f7a02968925a442
lucycopp <lucyanncopp@hotmail.co.uk>
2020-01-09 20:10:05 +0000

    retweets and likes things tagged with givaway

--- twitter/bot.js
+++ twitter/bot.js
@@ -1,20 +1,58 @@
 const twit = require('twit')
 const config = require('../config')
-
+const utils = require('../utils/sleep')
 const Twitter = new twit(config)
 
+const count = 100
 const params = {
     q: 'giveaway -filter:retweets -filter:replies',
-    result_type: 'mixed',
+    result_type: 'latest',
     lang: 'en',
-    count: 100
+    count: count
+}
+
+const likeTweet = tweetID => {
+    Twitter.post('favorites/create', {
+        id: tweetID
+    }, (err, response) => {
+        const output = response ? `Favourited tweet: ${tweetID}` : `Error favouriting ${err}`
+        console.log(output)
+    })
+}
+
+const retweet = tweetID => {
+    Twitter.post('statuses/retweet/:id', {
+        id: tweetID
+    }, (err, response) => {
+        const output = response ? `Tweeted tweet: ${tweetID}` : `Error favouriting ${err}`
+        console.log(output)
+    })
+}
+
+const handleTweet = (tweet, iteration) => {
+    if(typeof tweet != 'undefined') {
+        const id = tweet.id_str
+        retweet(id)
+        likeTweet(id)
+        console.log(`Completed ${iteration++}/${count}`)
+        utils.sleep(1000)
+    }
 }
 
 const runBot = () => {
     Twitter.get('search/tweets', params, function(err, data) {
-        console.log(data)
-    });
-  }
+        if(!err) {
+            const tweets = data.statuses
+            console.log(count)
+            for(let i = 0; i < count; i++) {
+               handleTweet(tweets[i], i)
+            }
+        } else {
+            console.log("ERROR")
+        }
+    })
+}
+    
 
 
 module.exports.run = runBot 
---
+++ utils/sleep.js
@@ -0,0 +1,10 @@
+//TEMPORARY FOR NOW
+
+const sleep = milliseconds => {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds) break;
+    }
+  }
+
+module.exports.sleep = sleep
